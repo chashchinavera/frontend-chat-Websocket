6590ee4b0c9b14a61b3646990b292ceffcf0a84d
Vera Chashchina <chaschinavera@gmail.com>
2024-10-13 15:18:34 +0300

    update Readme.md

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -1,4 +1,32 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import { io, Socket } from "socket.io-client";
+
+interface ServerToClientEvents {
+  noArg: () => void;
+  basicEmit: (a: number, b: string, c: Buffer) => void;
+  withAck: (d: string, callback: (e: number) => void) => void;
+}
+
+interface ClientToServerEvents {
+  hello: () => void;
+}
+const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io();
+
+ //const socket = io.connect("http://localhost:8000");
+
 const Chat = () => {
+  const { search } = useLocation();
+  const [params, setParams] = useState({});
+
+  useEffect(() => {
+    const searchParams = Object.fromEntries(new URLSearchParams(search));
+
+    console.log(searchParams);
+    setParams(searchParams);
+    socket("join", searchParams);
+  }, [search]);
+
   return <div>Chat</div>;
 };
 
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,27 +1,52 @@
+import { Link } from "react-router-dom";
+import { useState } from "react";
+
 const Main = () => {
+  const [values, setValues] = useState({
+    name: "",
+    room: "",
+  });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setValues({ ...values, [name]: value });
+  };
+
   return (
     <div className="main">
       <form className="form">
         <label className="label">Join Chat</label>
         <input
           type="text"
-          name="username"
-          value=""
+          name="name"
+          value={values.name}
           className="input"
-          onChange={() => {}}
+          onChange={handleChange}
           autoComplete="off"
           placeholder="Username"
+          required
         />
         <input
           type="text"
           name="room"
-          value=""
+          value={values.room}
           className="input"
-          onChange={() => {}}
+          onChange={handleChange}
           autoComplete="off"
           placeholder="Room"
-          
+          required
         />
+        <Link to={`/chat?name=${values.name}&room=${values.room}`}>
+          {values.name && values.room ? (
+            <button className="button" type="submit">
+              Sign in
+            </button>
+          ) : (
+            <button className="button" type="submit" disabled>
+              Sign in
+            </button>
+          )}
+        </Link>
       </form>
     </div>
   );
